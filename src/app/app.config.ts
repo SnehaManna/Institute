fc1e7a778448c80f24df7b669736e8e7e1c826cb
SnehaManna <snehamanna62@gmail.com>
2025-02-01 12:50:45 +0530

    user tab

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
-
+import {provideNativeDateAdapter} from '@angular/material/core';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
@@ -8,6 +8,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes,withComponentInputBinding()), 
-    provideAnimationsAsync()
+    provideAnimationsAsync(),
+    provideNativeDateAdapter()
   ]
 };
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,17 +1,19 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { ButtonComponent } from '../../shared/ui/button/button.component';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatIconModule } from '@angular/material/icon';
 import {
   FormControl,
   FormGroup,
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
-
+import { MatTabsModule } from '@angular/material/tabs';
 @Component({
   selector: 'app-register',
   standalone: true,
@@ -23,19 +25,25 @@ import {
     ButtonComponent,
     MatCheckboxModule,
     ReactiveFormsModule,
+    MatTabsModule,
+    MatDatepickerModule,
+    MatIconModule,
   ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
 })
 export class RegisterComponent implements OnInit {
   buttonType: string = 'primary';
   buttonLabel: string = 'Sign up';
-  signupForm!: FormGroup;
+  teacherSignupForm!: FormGroup;
+  studentSignupForm!: FormGroup;
   ngOnInit(): void {
-    this.signubuild();
+    this.teacherSignubuild();
+    this.studentSignubuild();
   }
-  signubuild() {
-    this.signupForm = new FormGroup({
+  teacherSignubuild() {
+    this.teacherSignupForm = new FormGroup({
       name: new FormControl('', [Validators.required]),
       email: new FormControl('', [Validators.required, Validators.email]),
       address: new FormControl('', [Validators.required]),
@@ -51,18 +59,54 @@ export class RegisterComponent implements OnInit {
         Validators.required,
         Validators.minLength(5),
       ]),
+      experience: new FormControl('', [
+        Validators.required,
+        Validators.maxLength(1),
+      ]),
+      subject: new FormControl('', [Validators.required]),
+    });
+  }
+  studentSignubuild() {
+    this.studentSignupForm = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      address: new FormControl('', [Validators.required]),
+      phone: new FormControl('', [
+        Validators.required,
+        Validators.minLength(10),
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(5),
+      ]),
+      confirmpassword: new FormControl('', [
+        Validators.required,
+        Validators.minLength(5),
+      ]),
+      DOB: new FormControl('', [Validators.required]),
+      class: new FormControl('', [Validators.required]),
     });
   }
   submit() {
-    if (this.signupForm.valid) {
-      console.log(this.signupForm.value);
-      localStorage.setItem('signup', JSON.stringify(this.signupForm.value));
+    if (this.teacherSignupForm.valid) {
+      console.log(this.teacherSignupForm.value);
+      localStorage.setItem(
+        'signup',
+        JSON.stringify(this.teacherSignupForm.value)
+      );
+    }
+    if (this.studentSignupForm.valid) {
+      console.log(this.studentSignupForm.value);
+      localStorage.setItem(
+        'signup',
+        JSON.stringify(this.studentSignupForm.value)
+      );
     }
   }
   get nameError() {
-    return this.signupForm.get('name');
+    return this.teacherSignupForm.get('name');
   }
   get emailError() {
-    return this.signupForm.get('email');
+    return this.teacherSignupForm.get('email');
   }
 }
