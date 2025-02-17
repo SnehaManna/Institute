010989d487d6de90d47cdd302afc459ff3a16e59
subhajit955 <subhajithalder955@gmail.com>
2025-02-17 19:45:51 +0530

    add header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
+import { DashboardComponent } from './pages/student/dashboard/dashboard.component';
 
 export const routes: Routes = [
     {
@@ -9,5 +10,6 @@ export const routes: Routes = [
         redirectTo: '/login'
       },
     {path:'login',component:LoginComponent},
-    {path:'signup',component:RegisterComponent}
+    {path:'signup',component:RegisterComponent},
+    {path:'dashboard', component: DashboardComponent}
 ];
